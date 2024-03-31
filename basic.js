2244a8b9ec91273ebd1670beb800fb17f726ded6
saifbrohi <saifullahbrohi65@gamil.com>
2024-03-31 15:18:10 +0500

    test msg

---
+++ basic.js
@@ -0,0 +1,14 @@
+const accountid = "12345"
+let accountemail = "saif@gmail.com"
+let accountpassword = "12345"
+let accountcity = "karachi"
+let accountstate;
+
+// console.log(accountid);
+
+accountemail = "ali@gmail.com"
+accountpassword = "444444"
+accountcity = "hyderabad"
+
+
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate])
