8fb68c852563ad00c4e6874e41ae13e883fbadb2
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-01 16:49:52 +0500

    operation

--- conversion.js
+++ conversion.js
@@ -20,6 +20,18 @@ let stringNumber = String(someNumber)
  console.log(typeof stringNumber);
  console.log(typeof (stringNumber));
 
+//****************operatiom********************//
+
+let str1 = 20
+let str2 = 30
+
+console.log("1" + 2);
+console.log("1" + 2 + 3);
+console.log( str1 + str2 + "2");
+
+let gamecounter=10
 
 
+gamecounter++;
 
+console.log(gamecounter);
