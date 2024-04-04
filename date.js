ea06e4539b3d67d857f4fabc3707593f9c33b418
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-04 14:37:51 +0500

    date

---
+++ date.js
@@ -0,0 +1,41 @@
+/// DATE /////
+
+
+let newData = new Date()
+console.log(newData.toString());
+
+console.log(newData.toDateString());
+
+console.log(newData.toISOString());
+
+console.log(newData.toJSON());
+
+console.log(newData.toLocaleString());
+
+ let gettime = new Date(2024,0,23)
+
+ let gettime = new Date(2024,0,24,5,2)
+
+let gettime = new Date("2024-02-12")
+
+ let createDate = new Date("01-12-2024")
+console.log(createDate.toLocaleString());
+
+console.log(createDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date()
+
+console.log(newdate);
+console.log(newdate.getMonth()+1);
+console.log(newdate.getDay());
+
+newdate . toLocaleString('default',{
+    weekday:"long",
+}
+
+)
