83a22839d13e4220cc748e64674e1a129a27c39f
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-07 15:29:11 +0500

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,45 @@
+const user ={
+    name: "SAIFULLAH",
+    price: 999,
+
+
+    welcomeMessage: function() { 
+        // console.log(`${this.name} , welcome to website`);
+        // console.log(this);
+    }
+}
+
+ user.welcomeMessage()
+ user.name = "sam"
+ user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let name ="SAIFULLAH"
+//     console.log(this.name);
+// }
+// chai()
+
+// function chai () => {
+//     let name ="SAIFULLAH"
+//     console.log(this.name);
+// }
+// chai()
+
+// const addone =(num1,num2) =>{
+//     return num1+num2
+// }
+// console.log(addone(3,4));
+
+// const addtwo =(num1,num2) => num1+num2
+
+// const addtwo =(num1,num2) => (num1+num2)
+
+const addtwo =(num1,num2) => {username:"SAIFULLAH"}
+
+console.log(addtwo(3,4));
+
+const myarr=[2,3,4,5,7]
+
+myarr.foreach()
