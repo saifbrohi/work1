72fd20137bb29d4195ff094a8fd0ad1893d2fd7e
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-07 14:06:33 +0500

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,21 +1,54 @@
 let a = 10
-// const b = 20                 ///////  SCOPE CODE 
+// const b = 20                 ///////  GLOBAL SCOPE CODE 
 //var  c = 300
 
 
 if (true) {
     let a = 10
     let oneplus = 78
-    const b = 20                ////// BLOCK SCOPE 
+    const b = 20                ////// LOCAL SCOPE 
     var c = 30
-    console.log("INNER LINE :",a)
+    //console.log("INNER LINE :",a)
 
    
 }
+//console.log(a);
 
+function one(){
+    const username="SAIFULLAH"
 
-// console.log(a);
-// console.log(b);
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
 
-console.log(a);
+    //console.log(website);
+    // two()
+}
+// one()
+
+if (true) {
+    const username = "SAIFULLAH"
+    if(username === "SAIFULLAH"){
+        const website=" YOUTUBE"
+        // console.log(username+website);
+    }
+
+    // console.log(website);
+   {
+        
+    }
+}
+
+//+++++++++++++++++++++++++ INTERESTING ==================
+
+console.log(addone(5));  ///////////////////// first method of when u dont need any Varaible name 
+function addone(num){
+    return num+1
+}
+
+// console.log(addtow(5)); ///////////////  when u declure the functiion but give variable 
+const addtow=function(num2){
+    return num2+2
+}
 
