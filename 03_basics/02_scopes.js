62c1a8444696b49a9ec3f5e9b187a2a311d9031a
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-07 12:11:40 +0500

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+let a = 10
+// const b = 20                 ///////  SCOPE CODE 
+//var  c = 300
+
+
+if (true) {
+    let a = 10
+    let oneplus = 78
+    const b = 20                ////// BLOCK SCOPE 
+    var c = 30
+    console.log("INNER LINE :",a)
+
+   
+}
+
+
+// console.log(a);
+// console.log(b);
+
+console.log(a);
+
