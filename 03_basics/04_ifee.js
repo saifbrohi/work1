18c30d535c968e2e0f63a2c89b9228f2ce91cb8b
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-07 23:15:48 +0500

    ifee

---
+++ 03_basics/04_ifee.js
@@ -0,0 +1,13 @@
+
+(function chai(){
+    console.log(`DB CONNECTED`);        /////////////////// IFEE IS PULOSHIHEN OF THE SCOPE ///
+} () );
+
+
+(function(name){
+    console.log(`DB COONNETED WITH JAVASCRIPT ${name}`); /////////////// USING FUNCTION WITH IFEE SYNTEX
+}("SAIFULLAH"));
+
+((name) =>{
+    console.log(`DB COONNETED WITH NODE JS ${name}`);  /////// USING ARROW METHOD AND WITHOIT FUNCTION NAME 
+})("SAIFULLAH")
