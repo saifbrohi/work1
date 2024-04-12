c2e59110f6b8684a7cb5a5c82bec048667a7b6fd
saifbrohi <saifullahbrohi65@gamil.com>
2024-04-12 13:22:33 +0500

    filter

---
+++ 05_basics/06_six.js
@@ -0,0 +1,49 @@
+const coding = ["java","node js","php","ruby"] 
+
+
+const values = coding.forEach( ( item ) => {
+        // console.log(item);
+        return item
+})
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const mynum = myarr.filter( ( num ) => num > 4)
+
+// const newNumbs = myarr.filter( ( num ) =>{
+//     return num > 4 
+// })
+// console.log(newarr);
+
+// const newNumbs =[]
+
+// myNums.forEach( ( num ) =>{
+//     if (num > 4) {
+//         newNumbs.push(num)
+//     }
+// })
+// console.log(newNumbs);
+
+    const books = [
+        { tittle:'book one',genre:'fiction',publish:1981,
+        edition:2004},
+
+        { tittle:'book two',genre:'non fiction',publish:1991,
+        edition:2006},
+        
+        { tittle:'book three',genre:'history',publish:1999,
+        edition:2000},
+        
+        { tittle:'book four',genre:'sciense',publish:2021,
+        edition:2002},
+        
+        { tittle:'book five',genre:'fiction',publish:1997,
+        edition:1999},
+        
+        { tittle:'book six',genre:'history',publish:1998,
+        edition:1995},
+        
+        { tittle:'book seven',genre:'sciense',publish:1990,
+        edition:1996},
+    ]
